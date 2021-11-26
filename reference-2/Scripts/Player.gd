c843586b2adc4fcb2edb81b643d6c30081faba6f
Kurtis Knodel <kurtisknodel@gmail.com>
2021-11-26 14:45:48 -0800

    Added mouse look

--- reference-2/Scripts/Player.gd
+++ reference-2/Scripts/Player.gd
@@ -1,10 +1,18 @@
 extends KinematicBody2D
 
 export(float) var move_speed = 64
+export(float) var rot_offset_degrees = 90
+
+onready var sprite = $Sprite
+onready var rot_offset = deg2rad(rot_offset_degrees)
 
 func _physics_process(_delta):
 	var movement = get_movement() * move_speed
 	var _v = self.move_and_slide(movement)
+	
+	# Make player face the mouse
+	var rotation = get_rotation()
+	sprite.rotation = rotation
 	
 	# Round position to avoid in-between pixel jitter
 	var pos = self.position
@@ -23,3 +31,7 @@ func get_movement():
 		m.y += 1
 	
 	return m.normalized()
+
+func get_rotation():
+	var mouse_pos = get_local_mouse_position()
+	return mouse_pos.angle() + rot_offset
